a6b8b07b85f8579a440f92f09f7fbcea1429ce4f
raylanb <56618618+raylanb@users.noreply.github.com>
2025-01-05 00:57:36 +0800

    fix-14

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,9 +6,9 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let context = tauri::generate_context!();
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .invoke_handler(tauri::generate_handler![your_command])
+        .run(context)
         .expect("error while running tauri application");
 }
