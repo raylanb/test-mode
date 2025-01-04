b2e04160010221b761f755a3ece11f77b50d67ed
raylanb <56618618+raylanb@users.noreply.github.com>
2025-01-05 01:13:52 +0800

    fix-15

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
-    tauri_build::build()
+    // 1. 调用 tauri_build 的构建脚本
+    tauri_build::build();
+
+    // 2. 设置移动端需要的环境变量
+    println!("cargo:rustc-env=TAURI_ANDROID_PACKAGE_NAME_PREFIX=com.test-mode.app");
+    println!("cargo:rustc-env=TAURI_ANDROID_PACKAGE_NAME_APP_NAME=test-mode");
 }
